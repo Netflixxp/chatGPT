144f06d488bbe7cc0aa2ea6d853a77a75eef51ee
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2023-02-13 17:33:09 +0800

    Update chat.sh

--- chat.sh
+++ chat.sh
@@ -5,6 +5,7 @@ PLAIN='\033[0m'
 GREEN='\033[0;32m'
 Yellow="\033[33m";
 log="unlock-chatgpt-test-result.log"
+local ip="$(ip addr show | grep -E '^[ ]*inet' | grep -m1 global | awk '{print $2}' | cut -d/ -f1)"
 
 clear;
 echo -e "${GREEN}** Chat GPT ip可用性检测${PLAIN} ${Yellow}by JCNF·那坨${PLAIN}" && echo -e "Chat GPT ip可用性检测 by JCNF·那坨" > ${log};
@@ -42,7 +43,6 @@ fi
 function UnlockChatGPTTest() {
     if [[ $(curl --max-time 10 -sS https://chat.openai.com/ -I | grep "text/plain") != "" ]]
     then
-        local ip="$(ip addr show | grep -E '^[ ]*inet' | grep -m1 global | awk '{print $2}' | cut -d/ -f1)"
         echo -e " 抱歉！本机IP：${ip}) ${RED}目前不支持ChatGPT IP is BLOCKED${PLAIN}" | tee -a $log
         return
     fi
@@ -54,7 +54,6 @@ function UnlockChatGPTTest() {
     if [ -n "$countryCode" ]; then
         support_countryCodes=(T1 XX AL DZ AD AO AG AR AM AU AT AZ BS BD BB BE BZ BJ BT BA BW BR BG BF CV CA CL CO KM CR HR CY DK DJ DM DO EC SV EE FJ FI FR GA GM GE DE GH GR GD GT GN GW GY HT HN HU IS IN ID IQ IE IL IT JM JP JO KZ KE KI KW KG LV LB LS LR LI LT LU MG MW MY MV ML MT MH MR MU MX MC MN ME MA MZ MM NA NR NP NL NZ NI NE NG MK NO OM PK PW PA PG PE PH PL PT QA RO RW KN LC VC WS SM ST SN RS SC SL SG SK SI SB ZA ES LK SR SE CH TH TG TO TT TN TR TV UG AE US UY VU ZM BO BN CG CZ VA FM MD PS KR TW TZ TL GB)
         if [[ "${support_countryCodes[@]}"  =~ "${countryCode}" ]];  then
-            local ip="$(ip addr show | grep -E '^[ ]*inet' | grep -m1 global | awk '{print $2}' | cut -d/ -f1)"
             echo -e " 恭喜！本机IP:${ip} ${GREEN}支持ChatGPT Yes (Region: ${countryCode})${PLAIN}" | tee -a $log
             return
         else
